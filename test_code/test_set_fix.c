540d2cd90589942e1db4a278220e61168b63a06e
wohenniubi <jiafan0420@tamu.edu>
2015-09-07 21:06:49 -0500

    CFSA 4D hardware test pass!
    Good!

--- test_code/test_set_fix.c
+++ test_code/test_set_fix.c
@@ -23,9 +23,9 @@
 uint16  fno;
 uint16  flist[4] = {0};
 
-int main()
+int main(int argc, char** argv)
 {
-    uint16  tunex1, tunex2;
+    uint16  tunex1, tunex2, tunex3, tunex4;
 
     if (init_mem()) return (1);
     if (init_cfg()) return (1);
@@ -33,16 +33,47 @@ int main()
 
     if (LoadCfgFile("./case1.cfg", &fno, flist)) return (1);
 
-    tunex1 = 16;
-    tunex2 = 13;
+    if (argc == 1)
+    {
+        tunex1 = 11;
+        tunex2 = 12;
 
-    printf("***** Setting X=%d, Y=%d\n", tunex1, tunex2);
+        printf("***** Setting X=%d, Y=%d\n", tunex1, tunex2);
 
-    SetTune_X1(tunex1);
-    SetTune_X2(tunex2);
-    SetTune_X3(15);
-    SetTune_X4(15);
-    TxCfg2ADC();
+        SetTune_X1(tunex1);
+        SetTune_X2(tunex2);
+        SetTune_X3(15);
+        SetTune_X4(15);
+        TxCfg2ADC();
+    }
+    else if (argc == 5)
+    {
+        tunex1 = atoi(argv[1]);
+        tunex2 = atoi(argv[2]);
+        tunex3 = atoi(argv[3]);
+        tunex4 = atoi(argv[4]);
+
+        printf("***** Setting X2=%d, Y2=%d, X1=%d, Y1=%d\n", tunex1, tunex2, tunex3, tunex4);
+
+        SetTune_X1(tunex1);
+        SetTune_X2(tunex2);
+        SetTune_X3(tunex3);
+        SetTune_X4(tunex4);
+        TxCfg2ADC();
+    }
+    else if (argc == 3)
+    {
+        tunex1 = atoi(argv[1]);
+        tunex2 = atoi(argv[2]);
+
+        printf("***** Setting X=%d, Y=%d\n", tunex1, tunex2);
+
+        SetTune_X1(tunex1);
+        SetTune_X2(tunex2);
+        SetTune_X3(15);
+        SetTune_X4(15);
+        TxCfg2ADC();
+    }
 
     return( clean_mem() );
 }
--- test_code/tune_adc_4d.c
+++ test_code/tune_adc_4d.c
@@ -17,11 +17,10 @@
 #include "../ana_opt_2/test_adc.h"
 
 #define ITERNUM 512
-#define SSNUM 7
+#define SSNUM 16
 #define VOFFSET 508
 #define AVG 8
 
-#define NEWD
 #define P_OPT
 //#define P_SWP
 #define P_FRP
@@ -29,9 +28,15 @@
 uint16  fno;
 uint16  flist[4] = {0};
 
-void TestCost(uint16 x1, uint16 x2, uint16 x3, uint16 x4, uint16*  adc_buf, uint16* cfh, uint16* cf)
+void  Test_CFSA_4D(uint16 x1, uint16 x2, uint16 x3, uint16 x4, uint16* adc_buf, uint16* anaz, uint16* cf)
 {
-    uint16 i;
+    uint16  i = 0, j = 0, k = 0;
+    uint16  adc_val;
+
+    for (i = 0; i < 18; i++)
+{
+        adc_buf[i] = 0;
+    }
 
     // Config
     SetTune_X1(x1);
@@ -39,25 +44,90 @@ void TestCost(uint16 x1, uint16 x2, uint16 x3, uint16 x4, uint16*  adc_buf, uint
     SetTune_X3(x3);
     SetTune_X4(x4);
 
-    // Get IQs
-    for (i=0; i<IQ_GRP_OFFSET+fno*4; i++) adc_buf[i] = 0;
-    FSELIQAvgRead(0, adc_buf, fno, flist, AVG);
-    for (i=0; i<IQ_GRP_OFFSET+fno*4; i++) adc_buf[i] /= AVG;
+    // Set TRG=1 just to make meas_adc_done keeps on high; so the verilog code could stay on ADC_RDY signal£»
+    CFSA4D_MUXTRG_Write(0);
+    CFSA4D_MUXTRG_Write(1);
 
-    // Calculate Costs
-    //*cf = GetCost4D_hw(adc_buf, VOFFSET);
-    *cfh = GetCost4D(adc_buf,VOFFSET);
-    *cf = *cfh;
+    CFSA4D_MUXRDY_Write(1);
+    CFSA4D_MUXRDY_Write(0);
 
+    SetFreq(flist[0]);
+
+    for (j = 0; j < 2; j++)
+    {
+        for (i = 0; i < AVG; i++)
+        {
+            adc_val = ReadCfgADC(0,j,0,1);//in current version, to get ADC needs to send config to the scan chain
+            adc_buf[j] += adc_val;
+            CFSA_ADCIN4D_Write(adc_val);
+            CFSA4D_ADCRDY_Write(1);
+            CFSA4D_ADCRDY_Write(0);
+        }
+    }
+    adc_buf[0] /= AVG;
+    adc_buf[1] /= AVG;
+
+    // wait CF begin 4X frequency measurment
+    for (i = 0; i < fno; i++)
+    {
+        // change FS frequency
+        SetFreq(flist[i]);
+
+        // wait till 4X signals' measurment is done
+        for (j = 0; j < 4; j++)
+        {
+            for (k=0; k < AVG; k++)
+            {
+                switch (j)
+                {
+                case 0:
+                    adc_val = ReadCfgADC(0,0,0,0); break;
+                case 1:
+                    adc_val = ReadCfgADC(0,1,0,0); break;
+                case 2:
+                    adc_val = ReadCfgADC(0,0,1,0); break;
+                case 3:
+                    adc_val = ReadCfgADC(0,1,1,0); break;
+                }
+                adc_buf[IQ_GRP_OFFSET + i*sizeof(FSEL_ELEMENT)/sizeof(IQ_ELEMENT) + j] += adc_val;
+
+                CFSA_ADCIN4D_Write(adc_val);
+                CFSA4D_ADCRDY_Write(1);
+                CFSA4D_ADCRDY_Write(0);
+            }
+
+            adc_buf[IQ_GRP_OFFSET + i*sizeof(FSEL_ELEMENT)/sizeof(IQ_ELEMENT) + j] /= AVG;
+
+            if ((0 == i) && (1 == j))
+            {
+                if (abs(adc_buf[0] - adc_buf[2])+abs(adc_buf[1] - adc_buf[3]) > OSC_TOL)
+                {
+                    *anaz = 255;
+                    *cf = 255;
+                    goto TESTCFSA_END;
+                }
+            }
+        }
+    }
+
+    usleep(50);
+    *anaz = CFSA_ANABITS_Read();
+    *cf = GetCFSA4D(adc_buf, VOFFSET, 1, 1, 0, 0, 0, 0);
+
+TESTCFSA_END:
+    usleep(80);
+
+    return;
 }
 
 int main()
 {
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    uint16  i = 0, j = 0;
+    uint16  i = 0, j = 0, k = 0, p = 0;
     uint16  tunex1, tunex2, tunex3, tunex4;
     uint16  anaz,cost;
-
+    uint16  gain0, gain1, gain2, gain3;
+    uint16  div_accu;
     uint16  adc_buf[384] = {0};
 
     /* Initialization */
@@ -72,106 +142,103 @@ int main()
         tunex2 = 15;
         tunex3 = 15;
         tunex4 = 15;
-        TestCost(tunex1, tunex2, tunex3, tunex4, adc_buf, &anaz, &cost);
 
+        gain0  = 0;
+        gain1  = 0;         // Gain factor for the second term
+        gain2  = 0;
+        gain3  = 0;
+        div_accu = 1;      // Set division accuracy is T27
 
-#ifndef NEWD
-        SA_INITX3_Write(tunex1);
-        SA_INITX4_Write(tunex2);
-        SA_ITERNUM_Write(ITERNUM);
-        WSA_4DY_Write(0);
-        SA_ANAIN_Write(anaz);
-        WSA_RSN_Write(1);
-#else
-        SA_INIT4DX3_Write(tunex1);
-        SA_INIT4DX4_Write(tunex2);
-        SA_INIT4DX1_Write(tunex3);
-        SA_INIT4DX2_Write(tunex4);
-        SA_ITER4DSA_Write(ITERNUM); // Set SA iteration = ITERNUM
-        SA_ITER4DSS_Write(SSNUM);       // Set Sensitivity time = 4
-        SA_INITT4D_Write(32);       // Set initial T = 32
-        SA4D_STEPT_Write(4);        // Set T step = 4; each time T=T-T_step;
-        SA4D_4DY_Write(1);          // Set 4D test
-        SA_ANAIN4D_Write(anaz);
-        SA4D_RSN_Write(1);
-#endif
+        CFSA4D_RSN_Write(0);
+        CFSA_3DBFACTOR0_Write(0x200);    // Set coefficiency for the 1st 3dB frequency point
+        CFSA_3DBFACTOR1_Write(0x200);    // Set coefficiency for the 2nd 3dB frequency point
+        CFSA_THRESHOLD_Write(100);  // Set threshold for multi-start SS
+        CFSA4D_GAIN0_Write(gain0);// 2D need to times 2
+        CFSA4D_GAIN1_Write(gain1);// 2D need to times 2
+        CFSA4D_GAIN2_Write(gain2);
+        CFSA4D_GAIN3_Write(gain3);
+        CFSA_INIT4DX1_Write(tunex1);
+        CFSA_INIT4DX2_Write(tunex2);
+        CFSA_INIT4DX3_Write(tunex3);
+        CFSA_INIT4DX4_Write(tunex4);
+        CFSA_ITER4DSA_Write(ITERNUM); // Set SA iteration = ITERNUM
+        CFSA_ITER4DSS_Write(SSNUM);       // SSNUM Set Sensitivity time = 4
+        CFSA_INITT4D_Write(32);       // Set initial T = 32
+        CFSA4D_STEPT_Write(4);        // Set T step = 4; each time T=T-T_step;
+        CFSA4D_4DY_Write(1);          // Set 4D test
+        CFSA4D_SARAND_Write(1);
+        CFSA4D_CFAVRG_Write(1);
+        CFSA4D_DIVACCU_Write(div_accu);
+        CFSA4D_MOD_Write(1);          // Optimization type
+        CFSA_TOLIN4D_Write(OSC_TOL);       // the OSCD threshold value;
+        CFSA_OFFIN4D_Write(VOFFSET);      // Offset used in IQ measurement
+
+        // TestCost(tunex1, tunex2, tunex1, tunex2, adc_buf, &anaz, &cost);
     }
 
     /* Find Optimal */
 #ifdef P_OPT
     {
+        CFSA4D_RSN_Write(1);        // activate the CFSA
+        usleep(2);
+
         printf("*** Optimization, ITERNUM=%d, SSNUM=%d\n", ITERNUM, SSNUM);
         printf("No.\tX2\tY2\tX1\tY1\tCFH\tCF\n");
-        printf("#0\t%d\t%d\t%d\t%d\t%d\t%d\n", tunex1, tunex2, tunex3, tunex4, anaz, cost);
 
-        for (i = 0; i < ITERNUM+SSNUM; i++)
+        for (i = 0; i < ITERNUM+SSNUM+1; i++)
         {
-#ifndef NEWD
-            SA_ANAIN_Write(anaz);
-            WSA_STA_Write(1);
-            WSA_STA_Write(0);
-            while(!WSA_RDY_Read());
-            tunex1 = SA_TUNEX3_Read();
-            tunex2 = SA_TUNEX4_Read();
-#else
-            SA_ANAIN4D_Write(anaz);
-            SA4D_STA_Write(1);
-            SA4D_STA_Write(0);
-            if (i<ITERNUM)
-            {
-                while(!SA4D_RDY_Read());
-            }
-            else
-            {
-                usleep(300);
-            }
-            tunex1 = SA_TUNE4DX3_Read();
-            tunex2 = SA_TUNE4DX4_Read();
-            tunex3 = SA_TUNE4DX1_Read();
-            tunex4 = SA_TUNE4DX2_Read();
-#endif
+            usleep(1);
+            tunex1 = CFSA_TUNE4DX1_Read();
+            tunex2 = CFSA_TUNE4DX2_Read();
+            tunex3 = CFSA_TUNE4DX3_Read();
+            tunex4 = CFSA_TUNE4DX4_Read();
 
-            TestCost(tunex1, tunex2, tunex3, tunex4, adc_buf, &anaz, &cost);
+            Test_CFSA_4D(tunex1, tunex2, tunex3, tunex4, adc_buf, &anaz, &cost);
+            CFSA4D_ANABITS_Write(anaz);// write back to CFSA
+
+            CFSA4D_MUXNXT_Write(1);
+            CFSA4D_MUXNXT_Write(0);// for safty
 
             printf("#%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i+1, tunex1, tunex2, tunex3, tunex4, anaz, cost);
+
             if (anaz<=1)
             {
-                i = ITERNUM-1;
+                i = ITERNUM+1;
             }
         }
 
-#ifndef NEWD
-        SA_ANAIN_Write(anaz);
-        WSA_STA_Write(1);
-        WSA_STA_Write(0);
-        tunex1 = SA_TUNEX3_Read();
-        tunex2 = SA_TUNEX4_Read();
-#else
-        SA_ANAIN4D_Write(anaz);
-        SA4D_STA_Write(1);
-        SA4D_STA_Write(0);
-        tunex1 = SA_TUNE4DX3_Read();
-        tunex2 = SA_TUNE4DX4_Read();
-        tunex3 = SA_TUNE4DX1_Read();
-        tunex4 = SA_TUNE4DX2_Read();
-#endif
+        /// set the optimal solution to analog circuit
+        tunex1 = CFSA_TUNE4DX1_Read();
+        tunex2 = CFSA_TUNE4DX2_Read();
+        tunex3 = CFSA_TUNE4DX3_Read();
+        tunex4 = CFSA_TUNE4DX4_Read();
+
+        Test_CFSA_4D(tunex1, tunex2, tunex3, tunex4, adc_buf, &anaz, &cost);
+        //CFSA4D_ANABITS_Write(anaz);
 
         printf("Optimization Final, X2=%d, Y2=%d, X1=%d, Y1=%d\n", tunex1, tunex2, tunex3, tunex4);
     }
 #endif // P_OPT
 
-    /* Scan Surface 2D */
+    /* Scan Surface 4D */
 #ifdef P_SWP
     {
         printf("*** Surface Scan\n");
-        printf("X\tY\tCFH\tCF\n");
+        printf("X2\tY2\tX1\tY1\tCFH\n");
 
         for (i=0; i<32; i++)
         {
             for (j=0; j<32; j++)
             {
-                TestCost(i, j, i, j, adc_buf, &anaz, &cost);
-                printf("%d\t%d\t%d\t%d\n", i, j, anaz, cost);
+                for (k=0; k<32; k++)
+                {
+                    for (p=0; p <32; p++)
+                    {
+                        Test_CFSA_4D(i, j, k, p, adc_buf, &anaz, &cost);
+                        // CFSA4D_ANABITS_Write(anaz);
+                        printf("%d\t%d\t%d\t%d\t%d\n", i, j, k, p, anaz);
+                    }
+                }
             }
         }
     }
@@ -207,13 +274,17 @@ int main()
                    MAG(A->Iout-VOFFSET, A->Qout-VOFFSET)/
                    MAG(A->Iinn-VOFFSET, A->Qinn-VOFFSET));
         }
-
-
-        SetFreq(72);
-        TxCfg2ADC();
-
     }
 #endif // P_FRP
+
+    printf("***** Setting X2=%d, Y2=%d, X1=%d, Y1=%d\n", tunex1, tunex2, tunex3, tunex4);
+
+    SetTune_X1(tunex1);
+    SetTune_X2(tunex2);
+    SetTune_X3(tunex3);
+    SetTune_X4(tunex4);
+    TxCfg2ADC();
+    //Global_RSTN_Write(0);
 
     return( clean_mem() );
 }
